6e8d2187c87b5b6cfca8a98b82afcb0e1fde8cce
Evellyn Rafael <71461331+EvellynRafael@users.noreply.github.com>
2025-02-19 12:46:43 -0300

    Create fichaParticipante.dart

---
+++ fichaParticipante.dart
@@ -0,0 +1,17 @@
+void main(){
+  String nome = "Evellyn Rafael";
+  int idade = 26;
+  String cpf = "100.000.000-00";
+  double altura = 1.56;
+  bool comunidade = true;
+
+  void ficha(){
+    print(
+    'Eu sou $nome, \n'
+    'Meu CPF é $cpf, \n'
+    'Sou membro da comunidade? $comunidade, \n'
+    'Eu tenho $altura de altura e $idade anos de idade');
+  }
+
+  ficha();
+}
